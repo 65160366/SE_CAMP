f082f757be073a39c60441ff721fb10fe9d1e1b4
65160366 <152172480+65160366@users.noreply.github.com>
2023-12-22 00:41:22 +0700

    Update form_101.php

--- form_101.php
+++ form_101.php
@@ -8,12 +8,15 @@
    // http://localhost/SE_CAMP/form_101.php?q=1
    // POST
    ?>
-   <form method = "post">
+   <form action = "" method = "">
       <input type = "text" name = "my_val" />
       <button type = "submit">บันทึก</button>
    </form>
-   <h1><?php if(isset($_POST['my_val'])){
+   <h1><?php 
+   print_r($_REQUEST);
+   if(isset($_POST['my_val'])){
       echo $_POST['my_val'];
    } ?></h1>
 </body>
+
 </html>
