f8c0ef40e0140064430bb8b70f1e1657aa9de49a
65160366 <152172480+65160366@users.noreply.github.com>
2023-12-21 17:41:13 +0700

    Create index_2.php

---
+++ index_2.php
@@ -0,0 +1,13 @@
+<html>
+
+<head>
+    <meta charset="utf-8">
+</head>
+
+<body>
+    <h1><?php echo "Hello World<br><br>"; ?></h1>
+    <?php echo "<h1>Hello World<br><br></h1>" ?>
+    <?php ?>
+</body>
+
+</html>
